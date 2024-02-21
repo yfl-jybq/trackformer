ef739f0fa9d4d468283fad27a9eb0b8f00f24b8c
yaofangling <yfl_jybq@163.com>
2024-02-21 11:40:42 +0800

    test.py

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import os 
 
-
+'''
 for i in range(18,29):
     with open("result.txt", "a") as f:
         f.writelines("id:"+str(i)+";")
-    '''
     path = '/home/yfl/passenger-flow-detection/yolov5-deepsort/'+'test_vedio/hualian_test_'+str(i)+'.mp4'
     os.system('rm -rf data/test2/*')
     os.system('ffmpeg -i '+path+' -vf fps=15 data/test2/%06d.png')
-    '''
-    input_name = '/home/yfl/passenger-flow-detection/yolov5-deepsort/'+'test_vedio/hualian_test_'+str(i)+'.mp4'
+    #input_name = '/home/yfl/passenger-flow-detection/yolov5-deepsort/'+'test_vedio/hualian_test_'+str(i)+'.mp4'
+    input_name = "test_1.mp4"
     output_name = 'result'+str(i)+'.mp4'
     os.system('python src/track.py with \
     dataset_name=DEMO \
@@ -18,15 +17,26 @@ for i in range(18,29):
     output_name='+output_name+' \
     input_name='+input_name+' \
     write_images=pretty')
+
+for i in range(13,14):
+    with open("result.txt", "a") as f:
+        f.writelines("id:"+str(i)+";")
+    input_name = '/home/yfl/Passenger-flow-detection/data'+'/test_'+str(i)+'.mp4'
+    output_name = 'result'+str(i)+'.mp4'
+    os.system('python src/track.py with \
+    dataset_name=DEMO \
+    data_root_dir=data/test2 \
+    output_dir=data/test \
+    output_name='+output_name+' \
+    input_name='+input_name+' \
+    write_images=pretty')
 '''
-input_name = '/home/yfl/passenger-flow-detection/yolov5-deepsort/'+'test_vedio/test'+'.mp4'
-output_name = 'result'+'.mp4'
 os.system('python src/track.py with \
 dataset_name=DEMO \
 data_root_dir=data/test2 \
-output_dir=data/test2 \
-output_name='+output_name+' \
-input_name='+input_name+' \
+output_dir=data/test \
+output_name=result_0221.mp4 \
+input_name=/home/yfl/Passenger-flow-detection/data/test_0221_seg.mp4 \
+poly_pass=1007,711,492,1076,2189,1274,2239,746 \
 write_images=pretty')
-'''
     
